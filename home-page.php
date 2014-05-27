034f66e26a884729bcf3c0ee08c088f1f89a53a6
Kartonnade <clouis.allizard@gmail.com>
2014-05-27 18:05:48 +0200

    remove actualités from home page

--- home-page.php
+++ home-page.php
@@ -7,10 +7,6 @@ Template Name: Home page
 <div class="small-12 large-8 columns" id="content" role="main">
 	
 		<?php get_template_part('templates/content', 'page'); ?>
-
-		<h1>Actualité récente</h1>
-		<hr>
-		<?php get_template_part('templates/loops', 'posts'); ?>
 		
 </div>
 <?php include basement_sidebar_path(); ?>	
